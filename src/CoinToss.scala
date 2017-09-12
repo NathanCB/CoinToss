cf79a8b83c8936d5389a10352151135a29da3aa1
NathanCB <nathancbutler@gmail.com>
2017-09-11 19:05:05 -0500

    update game

--- src/CoinToss.scala
+++ src/CoinToss.scala
@@ -14,12 +14,57 @@
 import scala.util.Random
 import scala.io.StdIn.readLine
 import CoinTossUtilities._
+import scala.annotation.tailrec
 
 case class GameState(tosses: Int, correctGuesses: Int)
 
 object CoinToss extends App{
-  val r = Random
-  val s = GameState
-  mainLoop(s, r)
+	val r = Random
+	val s = GameState(0, 0)
+
+	mainLoop(s, r)
+
+	@tailrec
+	def mainLoop(gameState: GameState, random: Random) = {
+			//prompt the user for input
+			showPrompt
+
+			//get the user's input
+			val userInput = getUserInput
+
+			userInput match {
+					case "H" | "T" => {
+  					//flip the coin
+  					val coinTossResult = coinToss(random)
+  					val newNumTosses = gameState.tosses + 1
   
+  					//compare the user's input with toss result
+  					if (userInput == coinTossResult) {
+  						//then user guessed right
+  						//write the output
+  					 mainLoop(newGameState, random)
+  					} 
+  					else {
+  					  //then user guessed wrong
+  					  //write the output
+  					  mainLoop(newGameState, random)
+  					} 
+					}
+					case _ => {
+					  //assume typed "Q"
+					  printGameOver
+					  printCurrentGameState(gameState)
+					  //exit game
+					}
+			}
+
+					//write the output
+
+					//if user hasn't typed "q" then loop again
+					mainLoop(newGameState, random)
+	}
+
 }
+
+
+
